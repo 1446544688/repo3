f0ffe80bdfc58d4f450b993cb0988b66f1238a29
jxj <1446544688@qq.com>
2020-06-28 15:17:22 +0800

    将java工程添加到版本库

---
+++ helloworld/src/main/java/com/jxj/HelloWorldMainApplication.java
@@ -0,0 +1,16 @@
+package com.jxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*
+* @SpringBootApplication 来标注一个主程序类，说明这是一个spring boot应用
+* */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        //spring 应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
